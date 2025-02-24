7610bb00307e76ac5c3199bb2c55a66b69ae4752
danisigal <dani.sigal1206@gmail.com>
2025-02-24 13:24:08 +0200

    sponsor and youth group database activities

---
+++ app/src/main/java/com/example/sponder/SponsorDB.java
@@ -0,0 +1,74 @@
+package com.example.sponder;
+
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class SponsorDB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_sponser_db);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    public static class Sponsor {
+        private String sponsorName;
+        private int amount;
+        private String email;
+        private String place;
+
+        public Sponsor() {
+            // Default constructor required for calls to DataSnapshot.getValue(Sponsor.class)
+        }
+
+        public Sponsor(String sponsorName, int amount, String email,String place) {
+            this.sponsorName = sponsorName;
+            this.amount = amount;
+            this.email = email;
+            this.place = place;
+        }
+
+        // Getters and setters
+        public String getSponsorName() {
+            return sponsorName;
+        }
+
+        public void setSponsorName(String sponsorName) {
+            this.sponsorName = sponsorName;
+        }
+
+        public int getAmount() {
+            return amount;
+        }
+
+        public void setAmount(int amount) {
+            this.amount = amount;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getPlace() {
+            return place;
+        }
+
+        public void setPlace(String place) {
+            this.place = place;
+        }
+    }
+}
---
+++ app/src/main/java/com/example/sponder/YouthGroupDB.java
@@ -0,0 +1,75 @@
+package com.example.sponder;
+
+import android.os.Bundle;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class YouthGroupDB extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_youth_group_db);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+    }
+
+    // Moved outside of onCreate and renamed
+    public static class YouthGroup {
+        private String youthGroupName;
+        private String email;
+        private int amountRequest;
+        private String place;
+
+        public YouthGroup() {
+            // Default constructor required for calls to DataSnapshot.getValue(YouthGroup.class)
+        }
+
+        public YouthGroup(String youthGroupName, String email, int amountRequest, String place) {
+            this.youthGroupName = youthGroupName;
+            this.email = email;
+            this.amountRequest = amountRequest;
+            this.place = place;
+        }
+
+        // Getters and setters
+        public String getYouthGroupName() {
+            return youthGroupName;
+        }
+
+        public void setYouthGroupName(String youthGroupName) {
+            this.youthGroupName = youthGroupName;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public int getAmountRequest() {
+            return amountRequest;
+        }
+
+        public void setAmountRequest(int amountRequest) {
+            this.amountRequest = amountRequest;
+        }
+
+        public String getPlace() {
+            return place;
+        }
+
+        public void setPlace(String place) {
+            this.place = place;
+        }
+    }
+}
